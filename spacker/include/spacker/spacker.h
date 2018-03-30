12214aa33bde740cf9404b5fe39049d475d15fd4
Enn Michael <enntheprogrammer@gmail.com>
2018-03-31 00:56:05 +0200

    Finished spacker, perhaps needs some finishing touches

--- spacker/include/spacker/spacker.h
+++ spacker/include/spacker/spacker.h
@@ -1,43 +1,46 @@
 #pragma once
 
 #include "boost/gil/gil_all.hpp"
+#include "boost/property_tree/ptree.hpp"
 #include <string>
 #include <vector>
 
 namespace Spacker {
 
-struct PositionedView;
-
 using Image = boost::gil::rgba8_image_t;
 using ConstView = boost::gil::rgba8c_view_t;
 using View = boost::gil::rgba8_view_t;
 
-// TODO Extend this to have a constructor that takes a std::vector of images,
-// and scrap the pack_sprites function
+struct Sprite {
+        static Sprite load(std::string const& path);
+
+        ConstView const_view() const noexcept;
+        View view() noexcept;
+
+        int width() const noexcept;
+        int height() const noexcept;
+
+        std::string name;
+        Image image;
+};
+
+std::vector<Sprite> load_sprites(std::vector<std::string> const& paths);
+
 class SpriteSheet {
 public:
-        static SpriteSheet pack(std::vector<Image> const& sprites);
+        static SpriteSheet pack(std::vector<Sprite> const& sprites);
 
         void write_to(std::string const& base_path) const;
 
 private:
-        SpriteSheet(Image image, std::string json) noexcept;
+        SpriteSheet(Image image, boost::property_tree::ptree tree) noexcept;
 
-        View image_view() noexcept;
-        ConstView const_image_view() const noexcept;
+        View view() noexcept;
+        ConstView const_view() const noexcept;
 
         Image image_;
-        std::string json_;
+        boost::property_tree::ptree tree_;
 };
-
-/**
- * Currently, image loading only works with PNG images with
- * RGBA channels with 8 bits of depth.
- */
-Image load_image(std::string const& path);
-std::vector<Image> load_images(std::vector<char const*> const& paths);
-
-SpriteSheet pack_sprites(std::vector<Image> const& sprites);
 
 }
 
--- spacker/src/main.cpp
+++ spacker/src/main.cpp
@@ -2,39 +2,115 @@
 #include "utils.h"
 #include "boost/range/irange.hpp"
 #include "boost/range/adaptors.hpp"
-
-// TODO Rename *.hpp -> *.h
+#include "boost/program_options.hpp"
+#include "boost/variant.hpp"
+#include <string_view>
+#include <exception>
+#include <sstream>
+#include <optional>
+#include <iostream>
 
 /**
  * NOTE:
  * As spacker is being written, the sprite database currently contains
- * files that seem to be of approximately the same height, and this has
- * very much influenced the design of the packing algorithm.
- * The algorithm prefers stacking the sprites horizontally.
+ * files that seem to be of approximately the same height.
+ * The algorithm is designed to work for this case only.
  */
 
 namespace {
 
 namespace gil = boost::gil;
+namespace po = boost::program_options;
 
-auto parameters_vector(int argc, char** argv)
+using namespace std::string_literals;
+
+std::string description_text(po::options_description const& desc)
 {
-        return Spacker::Utils::range_to_vector(
-                boost::irange(1, argc)
-              | boost::adaptors::transformed([&](auto const i) -> char const*
-                                             { return argv[i]; }));
+        std::stringstream ss;
+        ss << desc;
+        return ss.str();
 }
+
+struct CommandLineOptions {
+        using HelpString = std::string;
+
+        static boost::variant<HelpString, CommandLineOptions> parse(int argc, char** argv)
+        {
+                auto const desc = options_description();
+                auto const vm = variables_map(po::command_line_parser(argc, argv)
+                                                     .options(desc)
+                                                     .positional(positional_options_description())
+                                                     .run());
+                
+                if (vm.count("help"s)
+                    || !vm.count("output-file"s)
+                    || !vm.count("input-files"s))
+                        return description_text(desc);
+
+                return CommandLineOptions {
+                        vm["output-file"s].as<std::string>(),
+                        vm["input-files"s].as<std::vector<std::string>>()
+                };
+        }
+
+        std::string output_path;
+        std::vector<std::string> input_paths;
+
+private:
+        template <class ParsedOptions>
+        static po::variables_map variables_map(ParsedOptions const& parsed_options)
+        {
+                po::variables_map vm;
+                po::store(parsed_options, vm);
+                return vm;
+        } 
+
+        static po::positional_options_description positional_options_description()
+        {
+                po::positional_options_description desc;
+                desc.add("input-files", -1);
+                return desc;
+        }
+
+        static po::options_description options_description()
+        {
+                po::options_description desc("Options");
+                desc.add_options()
+                        ("help", "show help screen")
+
+                        ("output-file,o",
+                         po::value<std::string>(),
+                         "specify base name for output files")
+
+                        ("input-files",
+                         po::value<std::vector<std::string>>(),
+                         "specify input files");
+
+                return desc;
+        }
+};
 
 }
 
 int main(int argc, char **argv)
 {
-        if (argc == 1)
-                return 0;
+        struct CommandLineVisitor : boost::static_visitor<void> {
+                void operator()(CommandLineOptions::HelpString const& help) const
+                {
+                        std::cout << help << '\n';
+                }
 
-        auto const parameters = parameters_vector(argc, argv);
-        auto const sprites = Spacker::load_images(parameters);
-        auto const sprite_sheet = Spacker::SpriteSheet::pack(sprites);
-        sprite_sheet.write_to("./out");
+                void operator()(CommandLineOptions const& options) const
+                {
+                        auto const sprites = Spacker::load_sprites(options.input_paths);
+                        auto const sprite_sheet = Spacker::SpriteSheet::pack(sprites);
+                        sprite_sheet.write_to(options.output_path);
+                }
+        };
+
+        auto const parsed_result = CommandLineOptions::parse(argc, argv);
+        boost::apply_visitor(
+                CommandLineVisitor(),
+                parsed_result);
 }
 
